90245f9026cfd0e0656576ae4e3d6455a6040a8f
Oliver Forral <intrepion@gmail.com>
2016-01-14 03:34:26 -0800

    First Page

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController
+{
+    /**
+     * @Route("/genus")
+     */
+    public function showAction()
+    {
+        return new Response('Under the Sea!');
+    }
+}
