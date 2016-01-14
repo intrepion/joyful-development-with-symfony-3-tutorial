08666174e44e5ca20f61f0d3245b35b3f665ecb6
Oliver Forral <intrepion@gmail.com>
2016-01-14 03:39:04 -0800

    Routing Wildcards

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -9,10 +9,10 @@ use Symfony\Component\HttpFoundation\Response;
 class GenusController
 {
     /**
-     * @Route("/genus")
+     * @Route("/genus/{genusName}")
      */
-    public function showAction()
+    public function showAction($genusName)
     {
-        return new Response('Under the Sea!');
+        return new Response('The genus: ' . $genusName);
     }
 }
